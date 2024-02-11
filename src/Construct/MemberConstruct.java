bf584900a6ab70e1fc74a0cb780c2fd0c7873651
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-02-11 20:48:53 +0900

    기본생성자

--- src/Construct/MemberConstruct.java
+++ src/Construct/MemberConstruct.java
@@ -5,7 +5,7 @@ public class MemberConstruct {
     int age;
     int grade;
 
-    MemberConstruct(String name, int age, int grade) {
+    MemberConstruct(String name, int age, int grade) { // 생성자가 하나라도 있으면 기본 생성자가 제공되지 않는다.
         System.out.println("생성자 호출 name=" + name + ",age=" + age + ",grade=" + grade);
         this.name = name;
         this.age = age;
